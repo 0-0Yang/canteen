10503d516400052645676e7ca7e0b4ca42f354d3
房文同 <fangwentong2012@gmail.com>
2014-10-22 20:15:51 +0800

    配置数据库,完成反馈表单

--- models/order.py
+++ models/order.py
@@ -6,7 +6,22 @@ import web
 
 render = setting.render
 
-
 class index:
 	def GET(self):
 		return render.order.index("order", U"欢迎使用订餐系统")
+
+class signup:
+	def GET(self):
+		return render
+	def POST(self):
+		info = web.data()
+
+class add_order:
+	def GET(self):
+		return render.order.add_order("order", U"添加订单")
+	def POST(self):
+		web.seeother("/order/info")
+
+class get_info:
+	def GET(self):
+		return render.order.orderinfo("order", "订单信息")
